e8292a8ef50ecdfb735267f85bb7334c45fe8889
oliveriopt <oliver.pozo.m@gmail.com>
2025-07-22 11:04:16 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -22,3 +22,22 @@ def build_connection_string(config):
         f"Encrypt=yes;"
         f"TrustServerCertificate=yes;"
     )
+
+
+
+def run_pipeline():
+    pipeline_options = PipelineOptions(
+        runner='DataflowRunner',
+        project='rxo-dataeng-datalake-np',
+        region='us-central1',
+        temp_location='gs://rxo-dataeng-datalake-np-dataflow/temp',
+        staging_location='gs://rxo-dataeng-datalake-np-dataflow/staging',
+        save_main_session=True,
+        service_account_email='ds-dataflow-dataeng-gsa@rxo-dataeng-datalake-np.iam.gserviceaccount.com',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/nxo-corp-infra/regions/us-central1/subnetworks/rxo-dataeng-datalake-np-uscentral1',
+        use_public_ips=False,
+        worker_harness_container_image='us-central1-docker.pkg.dev/rxo-dataeng-datalake-np/dataflow-flex-template/sql-parquet-to-gcs-batched-ap:latest'
+    )
+    custom_options = pipeline_options.view_as(CustomPipelineOptions)
+    config = get_sql_config(custom_options.secret_id, custom_options.gcp_project)
+    connection_string = build_connection_string(config)
